4273c4fca7b5b3001585acff55154fc608f9f1c4
arkeyve <arkeyve@gmail.com>
2022-05-19 19:23:13 +0530

    typing animation

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const typingSpeed = 100;
+
+const animateTyping = (inputText, ele) => {
+  return new Promise((resolve, reject) => {
+    const inputTextLen = inputText.length;
+    let stepCounter = 0;
+    let stepTimer = setInterval(() => {
+      ele.innerHTML += inputText[stepCounter++];
+      if (stepCounter >= inputTextLen) {
+        clearInterval(stepTimer);
+        resolve();
+      }
+    }, typingSpeed);
+  });
+}
+
+const animateBackspace = (ele, backSpaceCount) => {
+  return new Promise((resolve, reject) => {
+    let stepTimer = setInterval(() => {
+      ele.innerHTML = ele.innerHTML.slice(0, -1);
+      backSpaceCount--;
+      if (backSpaceCount <= 0) {
+        clearInterval(stepTimer);
+        resolve();
+      }
+    }, typingSpeed + 100);
+  });
+}
+
+const animateName = (ele) => {
+  const nameTextS1 = 'leaf';
+  const nameTextS2 = 'pratyay amrit';
+
+  animateTyping(nameTextS1, ele).then(() => {
+    animateBackspace(ele, 4).then(() => {
+      animateTyping(nameTextS2, ele);
+    })
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  animateName(document.getElementById('nameheader'));
+});
