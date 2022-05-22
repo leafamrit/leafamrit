aac4a57e533c8a4e952912488cecb8de70b089fe
arkeyve <arkeyve@gmail.com>
2022-05-22 16:11:07 +0530

    new loading animation

--- js/main.js
+++ js/main.js
@@ -27,19 +27,51 @@ const animateBackspace = (ele, backSpaceCount) => {
   });
 }
 
-const animateName = (ele) => {
-  const nameTextS1 = 'leaf';
-  const nameTextS2 = 'pratyay amrit';
+const animateName = ele => {
+  return new Promise((resolve, reject) => {
+    const nameTextS1 = 'leaf am';
+    const nameTextS2 = 'pratyay amrit';
 
-  animateTyping(nameTextS1, ele).then(() => {
-    animateBackspace(ele, 4).then(() => {
-      animateTyping(nameTextS2, ele);
-    })
+    animateTyping(nameTextS1, ele).then(() => {
+      animateBackspace(ele, 7).then(() => {
+        animateTyping(nameTextS2, ele).then(() => {
+          resolve();
+        });
+      })
+    });
   });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  animateName(document.getElementById('nameheader'));
+const completeLoadAnimation = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.querySelector('.sidebar').classList.add('clickable');
+      document.querySelector('.clickable').addEventListener('click', () => {
+        let sidebar = document.querySelector('.sidebar');
+        sidebar.style.opacity = 0;
+        setTimeout(() => {
+          document.querySelector('.main-container-before').setAttribute('class', 'main-container-after');
+          document.documentElement.style.setProperty('--avatar-size', '200px');
+          document.querySelector('.soc-media').classList.remove('hidden');
+          setTimeout(() => {
+            sidebar.style.borderRadius = '20px';
+            sidebar.style.opacity = 0.9;
+            document.querySelector('.namediv').classList.remove('hidden');
+            resolve();
+          }, 350);
+        }, 350);
+      });
+    }, 2400);
+  })
+}
 
-  // document.documentElement.style.setProperty('--avatar-size', '300px')
+document.addEventListener('DOMContentLoaded', () => {
+  completeLoadAnimation().then(() => {
+    animateName(document.getElementById('nameheader')).then(() => {
+      document.querySelector('.details').classList.remove('hidden');
+      setTimeout(() => {
+        document.querySelector('.details>p').style.opacity = 1;
+      }, 100);
+    });
+  });
 });
